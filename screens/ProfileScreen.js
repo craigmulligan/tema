f57b0da449d4231f74e42238702633f6e5e90c8c
Craig Mulligan <hobochildster@gmail.com>
2020-05-06 17:33:17 +0200

    add basic auth

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,43 @@
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { auth } from '../firebase'
+
+export default function LinksScreen() {
+  return (
+    <View style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <Text>{auth.currentUser.email}</Text>
+      <Button title="logout" onPress={() => auth.signOut()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  },
+  optionIconContainer: {
+    marginRight: 12,
+  },
+  option: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderBottomWidth: 0,
+    borderColor: '#ededed',
+  },
+  lastOption: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  optionText: {
+    fontSize: 15,
+    alignSelf: 'flex-start',
+    marginTop: 1,
+  },
+});
