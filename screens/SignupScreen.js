9c208467d57730f29e424c2765476884290d57f5
Craig Mulligan <hobochildster@gmail.com>
2020-05-06 16:20:43 +0200

    add signup

---
+++ screens/SignupScreen.js
@@ -0,0 +1,71 @@
+import { Ionicons } from '@expo/vector-icons';
+import * as React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { auth } from '../firebase'
+
+export default function SignUpScreen({ navigation }) {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [error, setError] = React.useState({})
+  const [loading, setLoading] = React.useState(false)
+
+  return (
+    <View style={styles.container}>
+        {loading && <Text>Loading...</Text>}
+        {error.length > 0 && <Text>{error}</Text>}
+        <TextInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder={'Email'}
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setLoading(true)
+            auth.createUserWithEmailAndPassword(email, password)
+              .then(() => {
+                setLoading(false)
+                navigation.navigate('Root', { screen: 'Home' })
+              })
+              .catch(error => {
+                setLoading(false)
+                console.log('no!', error)
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                    setError('Weak Password')
+                } else {
+                    setError(errorMessage)
+                }
+              });
+          }}
+        ><Text style={styles.buttonText}>Signup</Text></TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    padding: 25,
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 25,
+  },
+  buttonText: {
+    color: 'white'
+  }
+});
