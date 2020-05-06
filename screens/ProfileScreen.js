45d4dda72e5f633c56edc064ee084d7ac8d229aa
Craig Mulligan <hobochildster@gmail.com>
2020-05-06 18:50:35 +0200

    use button for logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,26 @@
-import { Ionicons } from '@expo/vector-icons';
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
+import * as WebBrowser from 'expo-web-browser'
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import { auth } from '../firebase'
 import Button from '../primitives/Button'
 
 export default function LinksScreen() {
   return (
-    <View style={styles.container} contentContainerStyle={styles.contentContainer}>
-      <Text>{auth.currentUser.email}</Text>
-      <Button onPress={() => auth.signOut()} />Logout</Button>
+    <View
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <Text style={{ marginBottom: 25 }}>{auth.currentUser.email}</Text>
+      <Button onPress={() => auth.signOut()}>Logout</Button>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fafafa',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
-});
+})
